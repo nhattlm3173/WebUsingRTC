b6e85fe09a41faba04a659ee0ad7fe5aaef0b0d4
Thang Doan <katou2224@gmail.com>
2024-10-06 19:38:34 +0700

    final call

--- main.js
+++ main.js
@@ -6,41 +6,48 @@ const textUsername = document.getElementById("username");
 const uLUsers = document.getElementById("Users");
 const chatDiv = document.getElementById("divChat");
 const RegisterDiv = document.getElementById("divRegister");
+const header = document.getElementById("header");
+const searchUserInput = document.getElementById("searchUser");
+const listUser = [];
+header.style.display = "none";
 chatDiv.style.display = "none";
 socket.on("ONLINE_LIST", (arrUserInfo) => {
-  chatDiv.style.display = "block";
+  chatDiv.style.display = "flex";
+  header.style.display = "flex";
   RegisterDiv.style.display = "none";
   arrUserInfo.forEach((user) => {
     const { peerID, username } = user;
-    const li = document.createElement("li");
-    li.textContent = username;
-    li.id = peerID;
-    li.addEventListener("click", () => {
-      OpenStream().then((stream) => {
-        PlayStream("localStream", stream);
-        const call = peer.call(peerID, stream);
-        call.on("stream", (remoteStream) =>
-          PlayStream("remoteStream", remoteStream)
-        );
-      });
-    });
-    uLUsers.appendChild(li);
+    // const li = document.createElement("li");
+    // li.textContent = username;
+    // li.id = peerID;
+    // li.addEventListener("click", () => {
+    //   OpenStream().then((stream) => {
+    //     PlayStream("localStream", stream);
+    //     const call = peer.call(peerID, stream);
+    //     call.on("stream", (remoteStream) =>
+    //       PlayStream("remoteStream", remoteStream)
+    //     );
+    //   });
+    // });
+    // uLUsers.appendChild(li);
+    listUser.push({ username: username, id: peerID });
   });
   socket.on("HAVE_NEW_USER", (user) => {
     const { peerID, username } = user;
-    const li = document.createElement("li");
-    li.textContent = username;
-    li.id = peerID;
-    li.addEventListener("click", () => {
-      OpenStream().then((stream) => {
-        PlayStream("localStream", stream);
-        const call = peer.call(peerID, stream);
-        call.on("stream", (remoteStream) =>
-          PlayStream("remoteStream", remoteStream)
-        );
-      });
-    });
-    uLUsers.appendChild(li);
+    // const li = document.createElement("li");
+    // li.textContent = username;
+    // li.id = peerID;
+    // li.addEventListener("click", () => {
+    //   OpenStream().then((stream) => {
+    //     PlayStream("localStream", stream);
+    //     const call = peer.call(peerID, stream);
+    //     call.on("stream", (remoteStream) =>
+    //       PlayStream("remoteStream", remoteStream)
+    //     );
+    //   });
+    // });
+    // uLUsers.appendChild(li);
+    listUser.push({ username: username, id: peerID });
   });
   socket.on("SOMEONE_DISCONNECT", (peerID) => {
     console.log(peerID);
@@ -81,6 +88,8 @@ peer.on("open", function (id) {
   document.getElementById("peerId").innerText += id;
   buttonRegister.addEventListener("click", () => {
     const username = textUsername.value;
+    const usernameProfile = document.getElementById("usernameProfile");
+    usernameProfile.textContent = username;
     socket.emit("USER_REGISTER", { peerID: id, username: username });
   });
 });
@@ -103,4 +112,39 @@ peer.on("call", (call) => {
       PlayStream("remoteStream", remoteStream)
     );
   });
+});
+searchUserInput.addEventListener("input", function () {
+  const query = this.value.toLowerCase(); // Lấy giá trị nhập vào và chuyển sang chữ thường
+
+  // Xóa danh sách người dùng trước đó
+  uLUsers.innerHTML = "";
+
+  // Lọc và hiển thị người dùng theo truy vấn
+  const filteredUsers = listUser.filter(user => user.username.toLowerCase().includes(query));
+  console.log(query);
+  // Kiểm tra xem có người dùng nào phù hợp không
+  if (filteredUsers.length > 0) {
+    uLUsers.style.display = "block";
+    filteredUsers.forEach(user => {
+      const li = document.createElement("li");
+      li.textContent = user.username;
+      li.id = user.id;
+      li.addEventListener("click", () => {
+        OpenStream().then((stream) => {
+          PlayStream("localStream", stream);
+          const call = peer.call(user.id, stream);
+          call.on("stream", (remoteStream) =>
+            PlayStream("remoteStream", remoteStream)
+          );
+        });
+      });
+      uLUsers.appendChild(li); // Thêm vào danh sách hiển thị
+    });
+  }
+  else {
+    uLUsers.style.display = "none";
+  }
+  if (query === '') {
+    uLUsers.style.display = "none";
+  }
 });
